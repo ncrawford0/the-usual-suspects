e210533826cb1f14a05984671430dc791c511995
Neil Crawford Admin <ncrawfordadmin@lm-ncrawford.local>
2016-04-04 11:38:26 -0400

    Framework Commit

---
+++ bars/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bars::Application.config.secret_key_base = 'e1f108a405e2efb3c7939685cc881a4c21e3dd40ed0d760a31b1419a58970ef2d8a87a6d596c0fbfb554988df2acb18408f4aa5f5b9b232c8903704949135ac4'
