87e9734d8cda9fabb8c1605d6c305302c8ab982d
xnick421 <xnick421@gmail.com>
2016-04-05 16:54:04 -0400

    added reviews

---
+++ app/controllers/review_controller.rb
@@ -0,0 +1,53 @@
+class ReviewController < ApplicationController
+
+  def index
+    @reviews = Review.all
+  end
+
+  def show
+    @review = Review.find(params[:id])
+  end
+
+  def edit
+    @review = Review.find(params[:id])
+  end
+
+  def update
+    @review = Review.find(params[:id])
+    if @review.update_attributes(review_params)
+      #redirect and flash message
+    else
+      #render and flash message
+    end
+  end
+
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @bar = Bar.find(params[:bar_id])
+    @review = Review.new(review_params)
+    @review.bar = @bar
+    @review.user = current_user
+    if @review.save!
+      #redirect
+    else
+      #flash alert and render
+    end
+  end
+
+  def destroy
+    @review = Review.find(params[:id])
+    if @review.destroy!
+      #flash[:notice] = "Review deleted."
+      #redirect_to bars_path
+    else
+      #flahs and render
+    end
+  end
+
+  def review_params
+    params.require(:review).permit(:title, :body, :bar_id, :user_id, :rating)
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160405162120) do
+ActiveRecord::Schema.define(version: 20160405191647) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "reviews", force: :cascade do |t|
+    t.text     "title",      null: false
+    t.text     "body",       null: false
+    t.integer  "bar_id",     null: false
+    t.integer  "user_id",    null: false
+    t.integer  "rating",     null: false
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
