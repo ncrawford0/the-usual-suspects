955d730b73298080893b1f9a0cdabdc83860a196
Neil Crawford <ncrawford0@gmail.com>
2016-04-05 14:53:16 -0400

    Removed auto generated comments and fixed avatar test.

--- spec/features/avatar_spec.rb
+++ spec/features/avatar_spec.rb
@@ -1,4 +1,6 @@
 require "rails_helper"
+require "pry"
+
 
 feature "profile photo" do
   scenario "user uploads a profile photo" do
@@ -12,8 +14,8 @@ feature "profile photo" do
     visit "/users/edit"
 
     expect(page).to have_content("Edit User")
-    # expect(page).to have_xpath('//img[@src="/public/images/avatar.png"]')
-    # page.find('avatar')['src'].should have_content 'avatar.png'
-    expect(page).to have_css('.avatar')
+    image_file = page.find('.avatar')['src']
+    expect(image_file).to include ('avatar.jpg')
+    
   end
 end
