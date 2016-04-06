07489c62bf30852b07c83a99c3a08986d4a98f39
Matt McKelvey <mattmckelvey@Matts-MacBook-Pro-2.local>
2016-04-06 14:07:46 -0400

    fixed tests

--- spec/features/bars/user_views_bars_spec.rb
+++ spec/features/bars/user_views_bars_spec.rb
@@ -1,17 +1,13 @@
 require "rails_helper"
 
-feature "authenticated user views a list of bars" do
+feature "non-user views a list of bars" do
   scenario "sees all bars" do
 
-    new_user = User.create(email: "mckelvey.matt@gmail.com", password: "12345678")
     new_bar = Bar.create(name: "Beantown Pub", description: "Cool bar, guy.")
 
-    visit user_session_path
-    fill_in 'Email', with: 'mckelvey.matt@gmail.com'
-    fill_in 'Password', with: '12345678'
-    click_button "Log in"
+    visit bars_path
 
     expect(page).to have_content("All Bars")
-    expect(page).to have_content("Beantown Pub")
+    expect(page).to have_content(new_bar.name)
   end
 end
