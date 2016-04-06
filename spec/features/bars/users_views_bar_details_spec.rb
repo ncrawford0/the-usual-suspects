8f9a033718718bfc6192962134e29f24acaf6ec7
Neil Crawford <ncrawford0@gmail.com>
2016-04-06 16:23:48 -0400

    Fixed hound issues

--- spec/features/bars/users_views_bar_details_spec.rb
+++ spec/features/bars/users_views_bar_details_spec.rb
@@ -1,11 +1,9 @@
 require "rails_helper"
 
 feature "non-user views a bar's details" do
-
   let!(:bar1) { Bar.create(name: "Happy Hour Lasagna", description: "Where are we at?") }
 
   scenario "sees all details" do
-
     visit bars_path
     click_link "Happy Hour Lasagna"
 
