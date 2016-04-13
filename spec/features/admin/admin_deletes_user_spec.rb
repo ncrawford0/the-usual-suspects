cddbeb8fb30a73cae41f3ad0921f445cc65a31be
xnick421 <xnick421@gmail.com>
2016-04-13 16:03:54 -0400

    fixed hound issues

--- spec/features/admin/admin_deletes_user_spec.rb
+++ spec/features/admin/admin_deletes_user_spec.rb
@@ -4,17 +4,17 @@ feature "admin deletes a user" do
   let!(:new_admin) { Admin.create(email: "abcd@gmail.com", password: "12345678") }
   let!(:user1) { User.create(email: "abcd1@gmail.com", password: "12345678") }
   let!(:new_bar) { Bar.create(name: "underbar", description: "in the middle of downtown", user_id: user1.id) }
-  let!(:new_review) { Review.create(title: 'awesome!', body: "this place is the best!", rating: 5, bar_id: new_bar.id) }
+  let!(:new_review) { Review.create(title: "awesome!", body: "this place is the best!", rating: 5, bar_id: new_bar.id) }
 
-  scenario 'user is successfully deleted' do
-    visit '/admins/sign_in'
+  scenario "user is successfully deleted" do
+    visit "/admins/sign_in"
     fill_in "Email", with: "abcd@gmail.com"
     fill_in "Password", with: "12345678"
     click_button "Log in"
     click_link "Admin Dashboard"
     click_link "Edit Users"
 
-    find("#id-#{user1.id}").click_button('Delete')
+    find("#id-#{user1.id}").click_button("Delete")
     expect(page).to have_no_content(user1.email)
   end
-  end
+end
