571559a6cb2ab538a88b62c78c6c5428d197c043
Neil Crawford <ncrawford0@gmail.com>
2016-04-13 16:21:23 -0400

    Hound Fixes

--- app/mailers/review_mailer.rb
+++ app/mailers/review_mailer.rb
@@ -3,9 +3,9 @@ class ReviewMailer < ApplicationMailer
     @review = review
     @bar = bar
 
-    mail( from: "system@bevboston.com",
+    mail(from: "system@bevboston.com",
           to: "#{@bar.user.email}",
           subject: "#{@review.user.email} has added a comment to your #{@bar.name} page on bevboston.",
-          )
+        )
   end
 end
