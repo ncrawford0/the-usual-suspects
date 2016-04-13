4ee77bce17b4d93a0cd75036f92724641a1a402a
xnick421 <xnick421@gmail.com>
2016-04-13 14:40:50 -0400

    fixed hound issues

--- app/models/bar.rb
+++ app/models/bar.rb
@@ -5,8 +5,7 @@ class Bar < ActiveRecord::Base
   validates :name, presence: true
   validates :description, presence: true
 
-    def self.search(search)
-      where("name LIKE ?", "%#{search}%")
-    end
-
+  def self.search(search)
+    where("name LIKE ?", "%#{search}%")
+  end
 end
