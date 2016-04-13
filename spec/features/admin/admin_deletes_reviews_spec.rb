c94002849a574d2d0881cf3c073af63b37b2518e
xnick421 <xnick421@gmail.com>
2016-04-13 16:11:41 -0400

    fixed hound issues

--- spec/features/admin/admin_deletes_reviews_spec.rb
+++ spec/features/admin/admin_deletes_reviews_spec.rb
@@ -5,13 +5,15 @@ feature "admin deletes a bar" do
   let!(:user1) { User.create(email: "abcd1@gmail.com", password: "12345678") }
   let!(:new_bar) { Bar.create(name: "underbar", description: "in the middle of downtown", user_id: user1.id) }
   let!(:new_review) { Review.create(title: 'awesome!', body: "this place is the best!", rating: 5, bar_id: new_bar.id) }
-scenario "admin signs in and deletes bar" do
-  visit '/admins/sign_in'
-  fill_in "Email", with: "abcd@gmail.com"
-  fill_in "Password", with: "12345678"
-  click_button "Log in"
-  click_link "Admin Dashboard"
-  click_button "Delete underbar"
-  expect(page).to have_content("underbar has been deleted")
+
+  scenario "admin signs in and deletes bar" do
+    visit '/admins/sign_in'
+    fill_in "Email", with: "abcd@gmail.com"
+    fill_in "Password", with: "12345678"
+    click_button "Log in"
+    click_link "Admin Dashboard"
+    click_button "Delete underbar"
+
+    expect(page).to have_content("underbar has been deleted")
   end
 end
