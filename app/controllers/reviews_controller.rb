3eba4e5b9ceae7d74b41b4fd6278f7387caee1c0
Anthony DeGennaro <anthonydegennaro@Anthonys-MacBook.local>
2016-04-15 15:31:51 -0400

    last commit

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -40,16 +40,6 @@ class ReviewsController < ApplicationController
       flash[:notice] = "Review added"
       ReviewMailer.new_review(@review, @bar).deliver_later
       redirect_to bar_path(@bar)
-
-      twitter_client = Twitter::REST::Client.new do |config|
-        config.consumer_key = ENV["TWITTER_API_CONSUMER_KEY"]
-        config.consumer_secret = ENV["TWITTER_API_CONSUMER_SECRET"]
-        config.access_token = ENV["TWITTER_API_ACCESS_TOKEN"]
-        config.access_token_secret = ENV["TWITTER_API_ACCESS_SECRET"]
-      end
-
-      twitter_client.update("#{@bar.name}: \n #{@review.title}: #{@review.body}")
-
     else
       flash[:alert] = @review.errors.full_messages.join(". ")
       render "bars/show"
