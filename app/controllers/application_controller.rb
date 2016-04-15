01b56d06e46fcaf8cb288d8c4dde2f65e3714020
Neil Crawford <ncrawford0@gmail.com>
2016-04-15 13:14:02 -0400

    Activated twitter code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,5 +6,6 @@ class ApplicationController < ActionController::Base
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.for(:sign_up) << :avatar
+    devise_parameter_sanitizer.for(:account_update) << :avatar
   end
 end
